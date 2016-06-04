81f96143cd83925afcb7bb5569ae412d69609624
Bruno de Lima e Silva <dlimaun@gmail.com>
2016-06-04 18:17:10 -0300

    Update errors

--- src/main/java/com/github/brunodles/validationbuilder/Errors.java
+++ src/main/java/com/github/brunodles/validationbuilder/Errors.java
@@ -6,13 +6,17 @@ package com.github.brunodles.validationbuilder;
 public class Errors {
     // Object
     public static final int NULL = 1;
+    public static final int EQUAL = 2;
+
+    // Boolean
+    public static final int FALSE = 4;
+    public static final int TRUE = 8;
 
     // String
-    public static final int EMPTY = 2;
+    public static final int EMPTY = 16;
 
     // Integer
-    public static final int EQUAL = 8;
-    public static final int GREATER = 16;
-    public static final int LOWER = 32;
-    public static final int BETWEEN = 54;
+    public static final int GREATER = 32;
+    public static final int LOWER = 64;
+    public static final int BETWEEN = 128;
 }
