1d2c497835f0acc4eac6fae76cbbcd01970fb7ce
Bruno de Lima e Silva <dlimaun@gmail.com>
2016-06-07 00:44:44 -0300

    Create deploy script; tag: 0.10

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git config --global user.email "builds@travis-ci.com"
+git config --global user.name "Travis CI"
+
+export TAG=$(git log -1 | grep -Eo 'tag: ([0-9\.]+)' | cut -d' ' -f2)
+
+if [ -n "$TAG" ]; then
+    echo "$TAG">>.version
+    git commit -am "Prepare release $TAG"
+    git tag $TAG
+    git push origin $TAG
+    git push origin master
+fi
