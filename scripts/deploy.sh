64faba48660dd0372de782ae9a3e5fbddb20b9bf
Travis CI <Bruno de Lima>
2016-06-07 01:39:27 -0300

    Update deploy script to use environment variables; tag: 0.12

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-git config --global user.email "builds@travis-ci.com"
-git config --global user.name "Travis CI"
+if [ -n "$EMAIL" ]; then
+    git config user.email "$EMAIL"
+fi
+if [ -n "$PASSWORD" ]; then
+    git config user.name "Travis Ci"
+fi
 
 export TAG=$(git log -1 | grep -Eo 'tag: ([0-9\.]+)' | cut -d' ' -f2)
 
 if [ -n "$TAG" ]; then
+    git remote set-url origin "https://brunodles:${PASSWORD}@github.org/brunodles/java-validation.git"
     echo -n "$TAG">.version
     git commit -am "Prepare release $TAG"
     git tag $TAG
